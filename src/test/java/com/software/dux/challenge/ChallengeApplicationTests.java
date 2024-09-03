0653417dea52f7c0a026c377f04116228cab9a2e
Mariano Serrano <marianoeserrano15@gmail.com>
2024-09-02 21:26:37 -0300

    Crea proyecto SpringBoot

---
+++ src/test/java/com/software/dux/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.dux.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
